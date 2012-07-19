ce775a515b4cc9bee0d00a0b51b828ee85ccdabb
eivindw <eivindw@gmail.com>
2012-07-19 11:44:05 +0200

    Issue #1: Fix for kryo 2.16.

--- src/main/java/eivindw/io/KryoSerializer.java
+++ src/main/java/eivindw/io/KryoSerializer.java
@@ -1,14 +1,15 @@
 package eivindw.io;
 
 import com.esotericsoftware.kryo.Kryo;
-import com.esotericsoftware.kryo.ObjectBuffer;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
 
 public final class KryoSerializer {
 
    private static final Kryo kryo = new Kryo();
 
    static {
-      kryo.setRegistrationOptional(true);
+      kryo.setRegistrationRequired(false);
    }
    
    public static void register(Class... classes) {
@@ -18,12 +19,12 @@ public final class KryoSerializer {
    }
 
    public static byte[] write(Object obj) {
-      ObjectBuffer objectBuffer = new ObjectBuffer(kryo);
-      return objectBuffer.writeClassAndObject(obj);
+      Output output = new Output(512, 1024 * 8);
+      kryo.writeClassAndObject(output, obj);
+      return output.getBuffer();
    }
    
    public static Object read(byte[] bytes) {
-      ObjectBuffer objectBuffer = new ObjectBuffer(kryo);
-      return objectBuffer.readClassAndObject(bytes);
+      return kryo.readClassAndObject(new Input(bytes));
    }
 }
